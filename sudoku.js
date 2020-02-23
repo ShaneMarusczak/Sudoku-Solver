93086c00158c96209040208b916a08140a8932ee
ShaneMarusczak <marusczaks@gmail.com>
2020-02-23 15:42:18 -0500

    reorganizing code

--- sudoku.js
+++ sudoku.js
@@ -1,6 +1,6 @@
 "use strict";
 
-(function() {
+(function () {
 	const rows = 9;
 	const cols = 9;
 	const board = document.getElementById("sudoku");
@@ -8,7 +8,72 @@
 	const solution = document.getElementById("solution");
 	const copiedBoard = [];
 
-	const possible = function(y, x, n) {
+	for (let i = 0; i < rows; i++) {
+		copiedBoard.push([]);
+		const entryRow = document.createElement("div");
+		const solutionRow = document.createElement("div");
+		board.appendChild(entryRow);
+		solution.appendChild(solutionRow);
+		for (let j = 0; j < cols; j++) {
+			const entry = document.createElement("input");
+			const ans = document.createElement("input");
+			solutionRow.appendChild(ans);
+			entryRow.appendChild(entry);
+			entry.id = "s" + i + j;
+			ans.id = "a" + i + j;
+			ans.readOnly = true;
+			entry.type = "number";
+		}
+	}
+
+	const main = () => {
+		if (validate()) {
+			document.getElementById("invalidmessage").classList.add("hide");
+			document.getElementById("invalidmessage").classList.remove("show");
+		} else {
+			document.getElementById("invalidmessage").classList.remove("hide");
+			document.getElementById("invalidmessage").classList.add("show");
+			return;
+		}
+		for (let y = 0; y < rows; y++) {
+			for (let x = 0; x < cols; x++) {
+				copiedBoard[y][x] = document.getElementById("s" + y + x).value;
+			}
+		}
+		solve();
+		displayAnswer();
+	};
+
+	const validate = () => {
+		const nums = ["1", "2", "3", "4", "5", "6", "7", "8", "9"];
+		for (let y = 0; y < rows; y++) {
+			for (let x = 0; x < cols; x++) {
+				if (!nums.includes(document.getElementById("s" + y + x).value)) {
+					return false;
+				}
+			}
+		}
+		return true;
+	};
+
+	const solve = () => {
+		for (let y = 0; y < rows; y++) {
+			for (let x = 0; x < cols; x++) {
+				if (copiedBoard[y][x] == "") {
+					for (let n = 1; n < 10; n++) {
+						if (possible(y, x, n)) {
+							copiedBoard[y][x] = n;
+							solve();
+							copiedBoard[y][x] = "";
+						}
+					}
+					return;
+				}
+			}
+		}
+	};
+
+	const possible = (y, x, n) => {
 		for (let i = 0; i < 9; i++) {
 			if (copiedBoard[y][i] == n) {
 				return false;
@@ -31,56 +96,13 @@
 		return true;
 	};
 
-	const copy = () => {
+	const displayAnswer = () => {
 		for (let y = 0; y < rows; y++) {
 			for (let x = 0; x < cols; x++) {
-				copiedBoard[y][x] = document.getElementById("s" + y + x).value;
-			}
-		}
-	solve();
-	};
-
-	const solve = function() {
-		for (let y = 0; y < rows; y++) {
-			for (let x = 0; x < cols; x++) {
-				if (copiedBoard[y][x] == "") {
-					for (let n = 1; n < 10; n++) {
-						if (possible(y, x, n)) {
-							copiedBoard[y][x] = n;
-							solve();
-							copiedBoard[y][x] = "";
-						}
-					}
-					return;
-				}
-			}
-		}
-		for (let y = 0; y < rows; y++) {
-			for (let x = 0; x < cols; x++) {
-				document.getElementById("a" + y + x).innerHTML = copiedBoard[y][x];
+				document.getElementById("a" + y + x).value = copiedBoard[y][x];
 			}
 		}
 	};
-	solveBtn.addEventListener("click", copy);
 
-	for (let i = 0; i < rows; i++) {
-		copiedBoard.push([]);
-		const entryRow = document.createElement("div");
-		const solutionRow = document.createElement("div");
-		board.appendChild(entryRow);
-		solution.appendChild(solutionRow);
-		for (let j = 0; j < cols; j++) {
-			const entry = document.createElement("input");
-			const ans = document.createElement("input");
-			solutionRow.appendChild(ans);
-			entryRow.appendChild(entry);
-			entry.id = "s" + i + j;
-			ans.id = "a" + i + j;
-			ans.readOnly = true;
-			entry.type = "number";
-			entry.maxLength = 1;
-		}
-	}
-
-
+	solveBtn.addEventListener("click", main);
 })();
