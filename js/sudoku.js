be82acd73f0f45233bce01a027fe1082f00e9845
ShaneMarusczak <marusczaks@gmail.com>
2022-05-03 15:19:27 -0400

    Revert "general improvements"
    This reverts commit 484bde41663bf971a572816d5a5accfa03827aff.

--- js/sudoku.js
+++ js/sudoku.js
@@ -51,7 +51,7 @@
     if (counter === 0) {
       return false;
     }
-    let rv = true;
+    var rv = true;
     const validValues = ["1", "2", "3", "4", "5", "6", "7", "8", "9", ""];
     for (let y = 0; y < rows; y++) {
       for (let x = 0; x < cols; x++) {
@@ -75,6 +75,8 @@
     if (confirm("No solution was found in time. Clear entries and retry?")) {
       alert("Page will Reload.");
       setInterval(() => location.reload(), 1000);
+    } else {
+      return;
     }
   };
 
@@ -89,7 +91,7 @@
     }
     for (let y = 0; y < rows; y++) {
       for (let x = 0; x < cols; x++) {
-        if (copiedBoard[y][x] === "") {
+        if (copiedBoard[y][x] == "") {
           for (let n = 1; n < 10; n++) {
             if (possible(y, x, n)) {
               copiedBoard[y][x] = n;
@@ -111,12 +113,12 @@
 
   const possible = (y, x, n) => {
     for (let i = 0; i < 9; i++) {
-      if (copiedBoard[y][i] === n) {
+      if (copiedBoard[y][i] == n) {
         return false;
       }
     }
     for (let i = 0; i < 9; i++) {
-      if (copiedBoard[i][x] === n) {
+      if (copiedBoard[i][x] == n) {
         return false;
       }
     }
@@ -124,7 +126,7 @@
     const y0 = Math.floor(y / 3) * 3;
     for (let i = 0; i < 3; i++) {
       for (let j = 0; j < 3; j++) {
-        if (copiedBoard[y0 + i][x0 + j] === n) {
+        if (copiedBoard[y0 + i][x0 + j] == n) {
           return false;
         }
       }
